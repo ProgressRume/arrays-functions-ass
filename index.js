095bbddb230062c53fd97fb34a777d30b685ffbf
ProgressRume <awesomeluv94@gmail.com>
2023-03-07 13:41:56 +0100

    completion

--- index.js
+++ index.js
@@ -37,9 +37,22 @@ console.log(languages)
 
 fruit = apple, mango, orange
 
+// Question 4
+
 // Write a function that accepts an array of numbers as an argument. Return the maximum value in the array.
 // e.g., max([4, 5, 10, -2]) // maximum value is 10
 
 let numbers = [55, 2, 333];
 let maxNumbers = numbers.reduce((a, b) => Math.max(a, b), -Infinity);
 console.log('maximum value is ' + maxNumbers)
+
+// Question 5
+// Write a function called valTimesIndex which accepts an array of numbers and returns a new array with each value multiplied by the index it is at in the array:
+// e.g.,
+//  valTimesIndex([1,2,3]) // [0,2,6]
+// 		valTimesIndex([5,10,15]) // [0,10,30]
+
+let valTimesIndex = numArray => numArray.map((num, index) => num * index);
+
+console.log(valTimesIndex([1, 2, 3]));
+console.log(valTimesIndex([5, 10, 15]));
